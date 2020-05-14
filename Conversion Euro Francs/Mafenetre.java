8207d43b5f795dae5dd91340bfe9319669cb37e1
Manaxlle <49554886+Manaxlle@users.noreply.github.com>
2020-05-14 22:30:17 +0200

    Update Mafenetre.java

--- Conversion Euro Francs/Mafenetre.java
+++ Conversion Euro Francs/Mafenetre.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 /**
  * Décrivez votre classe Convertisseur ici.
  *
- * @author (votre nom)
+ * @Amir Nasseri (votre nom)
  * @version (un numéro de version ou une date)
  */
 public class Mafenetre extends JFrame implements ActionListener
